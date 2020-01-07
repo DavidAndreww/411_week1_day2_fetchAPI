c36680ad8909c9db8a76bd93600e627ac4364d39
David Bille <d_bille@yahoo.com>
2020-01-07 14:55:34 -0600

    fetched data and displaying by updating state and mapping over state. REACT IS SO COOL!

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 class App extends React.Component{
+  state = {
+    beers: []
+  }
+
+  fetchData = () => {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(parsedJSON => this.setState({beers: parsedJSON}))
+    .catch(error => console.log('Error occured: ', error))
+  } 
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
   render () {
     return (
       <div>
         <h1>Beer</h1>
         <p>api about beer!!!</p>
+        <ul>
+        {this.state.beers.map((beer, index) => (
+          <li key={beer.name}>Name: {beer.name}<br/>Tagling: {beer.tagline}</li>
+        ))}
+        </ul>
       </div>
     )
   }
