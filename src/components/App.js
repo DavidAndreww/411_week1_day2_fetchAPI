4aeaa2d750fe74cb3f031a79bf72c82c3200337f
David Bille <d_bille@yahoo.com>
2020-01-07 14:42:10 -0600

    set up framework for react app with fetch

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+  render () {
+    return (
+      <div>
+        <h1>Beer</h1>
+        <p>api about beer!!!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
