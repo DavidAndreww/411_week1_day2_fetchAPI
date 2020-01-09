aea65bd8c85a2aa80c3772f8d5d86784fd2523ed
David Bille <d_bille@yahoo.com>
2020-01-08 21:06:26 -0600

    like functionality almost working

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LikeButton = () => {
 return (
-  <p>Disliked</p>
+  <button>Like Me</button>
 )
 }
 
--- src/components/LikedButton.js
+++ src/components/LikedButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LikedButton = () => {
 return (
-  <p>LIKED</p>
+  <button>Check</button>
 )
 }
 
