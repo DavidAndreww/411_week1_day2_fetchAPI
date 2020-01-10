6327225c004530f15567ae73553808b6288338d1
David Bille <d_bille@yahoo.com>
2020-01-09 22:01:31 -0600

    ID bugs in comments

--- src/components/BeerItem.js
+++ src/components/BeerItem.js
@@ -1,3 +1,6 @@
+// state.isLiked changes on first call to toggleLike (line 20), but effects of class change are not visible on the DOM until called a second time. 
+
+
 import React from "react";
 import LikeButton from "./LikeButton";
 import UnlikeButton from "./UnlikeButton";
